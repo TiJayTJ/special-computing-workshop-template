da36f3e73211215567aa890ac891b541a97f1289
TiJayTJ <nurhantahaviev@gmail.com>
2023-11-28 21:58:53 +0300

    main structure of the MyArrayList class added

---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task2/arraylist/MyArrayList.java
@@ -0,0 +1,131 @@
+package ru.spbu.apcyb.svp.tasks.task2.arraylist;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyArrayList<T> implements List<T> {
+
+  @Override
+  public T set(int index, T element) {
+    // TODO: 28.11.2023
+    return null;
+  }
+
+  @Override
+  public void add(int index, T element) {
+    // TODO: 28.11.2023
+  }
+
+  @Override
+  public T remove(int index) {
+    // TODO: 28.11.2023
+    return null;
+  }
+  @Override
+  public boolean add(T t) {
+    // TODO: 28.11.2023
+    return false;
+  }
+
+  @Override
+  public T get(int index) {
+    // TODO: 28.11.2023
+    return null;
+  }
+
+  @Override
+  public int size() {
+    // TODO: 28.11.2023
+    return 0;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    // TODO: 28.11.2023
+    return false;
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    // TODO: 28.11.2023
+    return false;
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public Object[] toArray() {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public <T1> T1[] toArray(T1[] a) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public boolean remove(Object o) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public boolean containsAll(Collection<?> c) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends T> c) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public boolean addAll(int index, Collection<? extends T> c) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public boolean removeAll(Collection<?> c) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public boolean retainAll(Collection<?> c) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public void clear() {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public int indexOf(Object o) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public int lastIndexOf(Object o) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public ListIterator<T> listIterator() {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public ListIterator<T> listIterator(int index) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+
+  @Override
+  public List<T> subList(int fromIndex, int toIndex) {
+    throw new UnsupportedOperationException("unsupported operation");
+  }
+}
